eb51553ec56fdf3f4a50e20fde9d86d653fb61d0
lriccotti <nayy4033@gmail.com>
2021-11-26 15:46:29 -0300

    imagen

---
+++ Imagenv2}/MainForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VaCkensi
+ * Date: 18/11/2021
+ * Time: 12:32
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Imagenv2_
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pic1 = new System.Windows.Forms.PictureBox();
+			this.btn1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pic1
+			// 
+			this.pic1.Location = new System.Drawing.Point(63, 34);
+			this.pic1.Name = "pic1";
+			this.pic1.Size = new System.Drawing.Size(157, 153);
+			this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pic1.TabIndex = 5;
+			this.pic1.TabStop = false;
+			// 
+			// btn1
+			// 
+			this.btn1.Location = new System.Drawing.Point(84, 205);
+			this.btn1.Name = "btn1";
+			this.btn1.Size = new System.Drawing.Size(105, 55);
+			this.btn1.TabIndex = 6;
+			this.btn1.Text = "Mostrar ";
+			this.btn1.UseVisualStyleBackColor = true;
+			this.btn1.Click += new System.EventHandler(this.Btn1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(302, 323);
+			this.Controls.Add(this.btn1);
+			this.Controls.Add(this.pic1);
+			this.Name = "MainForm";
+			this.Text = "Imagenv2}";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Button btn1;
+		private System.Windows.Forms.PictureBox pic1;
+	}
+}
---
+++ Imagenv2}/MainForm.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VaCkensi
+ * Date: 18/11/2021
+ * Time: 12:32
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Imagenv2_
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		
+		string[] nombre_imagenes;
+		string  ubicacion_imagenes;
+		Random  valor_r;
+		int pic_mostrar;
+		
+		
+		public MainForm()
+		{
+			
+			
+			
+			InitializeComponent();
+			
+			
+		}
+		
+		void Btn1Click(object sender, EventArgs e)
+		{
+			valor_r=new Random();
+			pic_mostrar= valor_r.Next(0,5);
+			
+			pic1.Image= Image.FromFile(nombre_imagenes[pic_mostrar]);
+				
+			
+			
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			nombre_imagenes =new string[5];
+			ubicacion_imagenes="C:\\Users\\VaCkensi\\Pictures\\Tp_imagenesv2\\";
+			nombre_imagenes[0]=ubicacion_imagenes+"1.jpg";
+			nombre_imagenes[1]=ubicacion_imagenes+"2.jpg";
+			nombre_imagenes[2]=ubicacion_imagenes+"3.jpg";
+			nombre_imagenes[3]=ubicacion_imagenes+"4.jpg";
+			nombre_imagenes[4]=ubicacion_imagenes+"5.jpg";	
+
+			
+			
+			
+						
+		}
+		
+		
+	}
+}
